803fe146647249d54d5a7e55b15dc1db07a8f75c
peppecaliri <giuseppecaliri95@gmail.com>
2023-12-04 01:47:48 +0100

    fix

--- ElTimer/src/App.jsx
+++ ElTimer/src/App.jsx
@@ -5,13 +5,6 @@ import { TrainingContext } from "./TrainingContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Training from "./Training";
 
-//Purpouse: To create a timer for the gym, that notify you every rep, notify start and end of rest time, notify as well 5 seconds before the end of the rest time, and notify the end of the set.
-//The timer should be able to be setted by the user, so he can choose the number of sets, the number of reps, the type of reps (normal, double time, triple time), the rest time between sets.
-//The timer should be able to be paused and restarted.
-//The timer should be able to be resetted.
-//The timer should be able to be stopped.
-//The timer should be able to be setted again, after it has been stopped.
-
 function App() {
   const [trainingData, setTrainingData] = React.useState({
     sets: "",
